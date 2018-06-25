d0a873487059e39f0a18b227f7241abd5c9fe014
Alexa Scott <arscott4792@gmail.com>
2018-06-25 16:02:03 -0400

    bar graph attempt

--- components/reports.js
+++ components/reports.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Text, View, Dimensions } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import ReportsList from './reports-list';
 import HeaderMain from './HeaderMain';
-import { LineChart, Grid } from 'react-native-svg-charts'
+import { LineChart, Grid, BarChart } from 'react-native-svg-charts'
 import moment from 'moment';
 
 class Reports extends React.Component {
@@ -13,21 +13,48 @@ class Reports extends React.Component {
             month: ''
         }
     }
+
+    handlePress(e) {
+        console.log(e);
+    }
     render() {
-        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
-        // console.log('halp', this.props)
+        // const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
+        const svgs = [
+            { onPress: () => console.log('apples') }
+        ]
+        const keys   = [ 'apples', 'bananas', 'cherries', 'dates' ]
+       
+        const fill = 'rgb(134, 65, 244)'
+        const data   = [ 50, 10, 40, 95, -4, -24, null, 85, undefined, 0, 35, 53, -53, 24, 50, -20, -80 ]
         return (
         <View> 
             <HeaderMain />
             <Text>Insert line chart</Text>
+            <BarChart
+                style={{ height: 200 }}
+                data={ data }
+                // svg={{ fill }}
+                keys={ keys }
+                contentInset={{ top: 30, bottom: 30 }}
+                onPress={() => this.handlePress}
+                svgs = { svgs }
+            >
+                <Grid/>
+            </BarChart>
+
+
+            {/* <TouchableOpacity>
             <LineChart
                 style={{ height: 200 }}
                 data={ data }
                 svg={{ stroke: 'rgb(134, 65, 244)' }}
                 contentInset={{ top: 20, bottom: 20 }}
+                // svgs = { svgs }
+                onPressItem={() => console.log('hi')}
             >
-                <Grid/>
+                <Grid />
                 </LineChart>
+                </TouchableOpacity> */}
         </View>
         )
     }
